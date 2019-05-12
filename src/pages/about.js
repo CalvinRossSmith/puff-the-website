f03a39795906e8a83e2a0f98bd8a0e75209a2947
CalvinRossSmith <smithcalvin33@gmail.com>
2019-05-12 17:36:45 -0400

    fixed spelling mistakes

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,13 +12,13 @@ export default () => (
         </Helmet>
         <h1>About Me</h1>
         <p> 
-            My name is Calvin Smith and I am a second year Software Engineering student at the University of Ottawa.
+            My name is Calvin Smith. I am a second year Software Engineering student at the University of Ottawa.
             I have a drive for gaining a deeper understanding of how technology works. 
-            It is my passion to apply this knowledge that I have gained to solve interesting real world technology problems.
-            I have experience with a few amazing companies, like Espial Group and Ribbon Comminications, and also a variety of self-taught experience. 
+            It is my passion to apply this knowledge that I have gained, to solve interesting real world problems.
+            I have had experience with a few amazing companies, like Espial Group and Ribbon Communications as well as a variety of self-taught experiences. 
         </p>
         <p>    
-            If you want to discuss hiring me as a Co-op/Intern or part time software engineer, feel free to shoot me a message!
+            If you would like to discuss the possibility of hiring me as a co-op/intern or part time software engineer
         </p>
     </Layout>
     
